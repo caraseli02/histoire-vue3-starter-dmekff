9f9d245fdf11cde096416e3488221d27879e44e4
caraseli02 <caraseli02@gmail.com>
2022-10-15 11:19:00 +0200

    feat: add new btns storys

---
+++ src/buttons/PrimaryBtn.story.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+import { hstEvent } from 'histoire/client'
+import LoadingBtn from './LoadingBtn.vue'
+
+const text = ref('Click me')
+</script>
+
+<template>
+  <Story>
+    <LoadingBtn color="greenBlue" @click="hstEvent('click', $event)">
+      {{ text }}
+    </LoadingBtn>
+
+    <template #controls>
+      <HstText title="default slot" v-model="text" />
+    </template>
+  </Story>
+</template>
